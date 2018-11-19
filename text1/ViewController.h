d7641d12d79e5b1f6642ee10be985dfe68f8ba3f
lupanpan <lupanpan123456@163.com>
2018-11-19 10:39:38 +0800

    Initial Commit

---
+++ text1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  text1
+//
+//  Created by Admin on 2018/11/19.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ text1/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  text1
+//
+//  Created by Admin on 2018/11/19.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
