d5f1f6b731f74421d0b2c6e074312a9bddc60838
lupanpan <lupanpan123456@163.com>
2018-11-21 09:48:37 +0800

    a

---
+++ text1/OpenHourCellTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  OpenHourCellTableViewCell.m
+//  text1
+//
+//  Created by Admin on 2018/11/19.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+#import "OpenHourCellTableViewCell.h"
+
+@implementation OpenHourCellTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
